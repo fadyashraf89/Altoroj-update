54ce1c6ddc02cfd463ca16880b3ac5d298fb26f3
Fady Ashraf <106484095+fadyashraf89@users.noreply.github.com>
2024-05-04 02:42:12 +0300

    Add files via upload

--- src/main/java/com/ibm/security/appscan/altoromutual/model/User.java
+++ src/main/java/com/ibm/security/appscan/altoromutual/model/User.java
@@ -1,58 +1,43 @@
-/**
-This application is for demonstration use only. It contains known application security
-vulnerabilities that were created expressly for demonstrating the functionality of
-application security testing tools. These vulnerabilities may present risks to the
-technical environment in which the application is installed. You must delete and
-uninstall this demonstration application upon completion of the demonstration for
-which it is intended. 
-
-IBM DISCLAIMS ALL LIABILITY OF ANY KIND RESULTING FROM YOUR USE OF THE APPLICATION
-OR YOUR FAILURE TO DELETE THE APPLICATION FROM YOUR ENVIRONMENT UPON COMPLETION OF
-A DEMONSTRATION. IT IS YOUR RESPONSIBILITY TO DETERMINE IF THE PROGRAM IS APPROPRIATE
-OR SAFE FOR YOUR TECHNICAL ENVIRONMENT. NEVER INSTALL THE APPLICATION IN A PRODUCTION
-ENVIRONMENT. YOU ACKNOWLEDGE AND ACCEPT ALL RISKS ASSOCIATED WITH THE USE OF THE APPLICATION.
-
-IBM AltoroJ
-(c) Copyright IBM Corp. 2008, 2013 All Rights Reserved.
- */
 package com.ibm.security.appscan.altoromutual.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import com.ibm.security.appscan.Log4AltoroJ;
 import com.ibm.security.appscan.altoromutual.util.DBUtil;
+import com.ibm.security.appscan.altoromutual.util.*;
 
-/**
- * This class models a user
- * @author Alexei
- *
- */
-public class User implements java.io.Serializable{
+public class User implements java.io.Serializable {
 
 	private static final long serialVersionUID = -4566649173574593144L;
-	
-	public static enum Role{User, Admin};
-	
+
+	public static enum Role { User, Admin };
+
 	private String username, firstName, lastName;
 	private Role role = Role.User;
-	
 	private Date lastAccessDate = null;
-	
+
 	public User(String username, String firstName, String lastName) {
 		this.username = username;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		lastAccessDate = new Date();
 	}
-	
-	public void setRole(Role role){
+
+	public void setRole(Role role) {
 		this.role = role;
 	}
-	
-	public Role getRole(){
+
+	public Role getRole() {
 		return role;
 	}
-	
+
 	public Date getLastAccessDate() {
 		return lastAccessDate;
 	}
@@ -72,8 +57,8 @@ public class User implements java.io.Serializable{
 	public String getLastName() {
 		return lastName;
 	}
-	
-	public Account[] getAccounts(){
+
+	public Account[] getAccounts() {
 		try {
 			return DBUtil.getAccounts(username);
 		} catch (SQLException e) {
@@ -81,7 +66,7 @@ public class User implements java.io.Serializable{
 			return null;
 		}
 	}
-	
+
 	public Account lookupAccount(Long accountNumber) {
 		for (Account account : getAccounts()) {
 			if (account.getAccountId() == accountNumber)
@@ -89,19 +74,55 @@ public class User implements java.io.Serializable{
 		}
 		return null;
 	}
-	
-	public long getCreditCardNumber(){
-		for (Account account: getAccounts()){
+
+	public long getCreditCardNumber() {
+		for (Account account : getAccounts()) {
 			if (DBUtil.CREDIT_CARD_ACCOUNT_NAME.equals(account.getAccountName()))
 				return account.getAccountId();
 		}
 		return -1L;
 	}
-	
-	public Transaction[] getUserTransactions(String startDate, String endDate, Account[] accounts) throws SQLException {
-		
-		Transaction[] transactions = null;
-		transactions = DBUtil.getTransactions(startDate, endDate, accounts, -1);
-		return transactions; 
+
+	public Transaction[] getUserTransactions(String startString, String endString, Account[] accounts) {
+		try {
+			if (startString == null || endString == null || startString.isEmpty() || endString.isEmpty()) {
+				return null;
+			}
+
+			String query = "SELECT * FROM TRANSACTIONS WHERE (" + getAccountIdsCondition(accounts) + ") AND DATE BETWEEN ? AND ?";
+			Connection connection = DBUtil.getConnection();
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setString(1, startString + " 00:00:00");
+			statement.setString(2, endString + " 23:59:59");
+			ResultSet resultSet = statement.executeQuery();
+
+			ArrayList<Transaction> transactions = new ArrayList<>();
+			while (resultSet.next()) {
+				int transId = resultSet.getInt("TRANSACTION_ID");
+				long actId = resultSet.getLong("ACCOUNTID");
+				Timestamp date = resultSet.getTimestamp("DATE");
+				String desc = resultSet.getString("TYPE");
+				double amount = resultSet.getDouble("AMOUNT");
+				transactions.add(new Transaction(transId, actId, new Date(date.getTime()), desc, amount));
+			}
+
+			return transactions.toArray(new Transaction[0]);
+		} catch (SQLException e) {
+			Log4AltoroJ.getInstance().logError("Error retrieving user transactions: " + e.getMessage());
+			return null;
+		}
+	}
+
+	private String getAccountIdsCondition(Account[] accounts) {
+		StringBuilder condition = new StringBuilder();
+		condition.append("(");
+		for (int i = 0; i < accounts.length; i++) {
+			if (i > 0) {
+				condition.append(" OR ");
+			}
+			condition.append("ACCOUNTID = ?");
+		}
+		condition.append(")");
+		return condition.toString();
 	}
 }
--- src/main/java/com/ibm/security/appscan/altoromutual/servlet/AdminLoginServlet.java
+++ src/main/java/com/ibm/security/appscan/altoromutual/servlet/AdminLoginServlet.java
@@ -1,19 +1,19 @@
 /**
-This application is for demonstration use only. It contains known application security
-vulnerabilities that were created expressly for demonstrating the functionality of
-application security testing tools. These vulnerabilities may present risks to the
-technical environment in which the application is installed. You must delete and
-uninstall this demonstration application upon completion of the demonstration for
-which it is intended. 
-
-IBM DISCLAIMS ALL LIABILITY OF ANY KIND RESULTING FROM YOUR USE OF THE APPLICATION
-OR YOUR FAILURE TO DELETE THE APPLICATION FROM YOUR ENVIRONMENT UPON COMPLETION OF
-A DEMONSTRATION. IT IS YOUR RESPONSIBILITY TO DETERMINE IF THE PROGRAM IS APPROPRIATE
-OR SAFE FOR YOUR TECHNICAL ENVIRONMENT. NEVER INSTALL THE APPLICATION IN A PRODUCTION
-ENVIRONMENT. YOU ACKNOWLEDGE AND ACCEPT ALL RISKS ASSOCIATED WITH THE USE OF THE APPLICATION.
-
-IBM AltoroJ
-(c) Copyright IBM Corp. 2008, 2013 All Rights Reserved.
+ * This application is for demonstration use only. It contains known application security
+ * vulnerabilities that were created expressly for demonstrating the functionality of
+ * application security testing tools. These vulnerabilities may present risks to the
+ * technical environment in which the application is installed. You must delete and
+ * uninstall this demonstration application upon completion of the demonstration for
+ * which it is intended.
+ *
+ * IBM DISCLAIMS ALL LIABILITY OF ANY KIND RESULTING FROM YOUR USE OF THE APPLICATION
+ * OR YOUR FAILURE TO DELETE THE APPLICATION FROM YOUR ENVIRONMENT UPON COMPLETION OF
+ * A DEMONSTRATION. IT IS YOUR RESPONSIBILITY TO DETERMINE IF THE PROGRAM IS APPROPRIATE
+ * OR SAFE FOR YOUR TECHNICAL ENVIRONMENT. NEVER INSTALL THE APPLICATION IN A PRODUCTION
+ * ENVIRONMENT. YOU ACKNOWLEDGE AND ACCEPT ALL RISKS ASSOCIATED WITH THE USE OF THE APPLICATION.
+ *
+ * IBM AltoroJ
+ * (c) Copyright IBM Corp. 2008, 2013 All Rights Reserved.
  */
 package com.ibm.security.appscan.altoromutual.servlet;
 
@@ -39,17 +39,13 @@ public class AdminLoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String password = request.getParameter("password");
-		if (password == null){
-			response.sendRedirect(request.getContextPath()+"/admin/login.jsp");
-			return ;
-		} else if (!password.equals("Altoro1234")){
+		if (password == null || !password.equals("Altoro1234")) {
 			request.setAttribute("loginError", "Login failed.");
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/admin/login.jsp");
 			dispatcher.forward(request, response);
-			return;
 		} else {
 			request.getSession(true).setAttribute(ServletUtil.SESSION_ATTR_ADMIN_KEY, ServletUtil.SESSION_ATTR_ADMIN_VALUE);
-			response.sendRedirect(request.getContextPath()+"/admin/admin.jsp");
+			response.sendRedirect(request.getContextPath() + "/admin/admin.jsp");
 		}
 	}
 }
